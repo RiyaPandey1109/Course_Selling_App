9428243d6be2ed0aca23b67d98c7f59985e3a2ea
Riya Pandey <riyapandey221203@gmail.com>
2024-10-01 23:37:08 +0530

    Initial Commit

---
+++ Backend/Routes/Admin.js
@@ -0,0 +1,69 @@
+const { Router } = require("express");
+const adminMiddleware = require("../Middleware/admin");
+const router = Router();
+const {Admin, Course} = require("../db");
+const {user} = require("../db");
+
+// Admin Routes
+router.post('/signup', async(req, res) => {
+    // Implement admin signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // Check if user with this username already exists 
+    await Admin.create({
+        username:username,
+        password:password
+    })
+    res.json({
+            message : "Admin created successfully"
+    })
+
+
+});
+
+// Input: Headers: { 'username': 'username', 'password': 'password' }, Body: { title: 'course title', description: 'course description', price: 100, imageLink: 'https://linktoimage.com' }
+//   Output: { message: 'Course created successfully', courseId: "new course id" }
+
+router.post('/courses', adminMiddleware, async (req, res) => {
+    // Implement course creation logic
+
+    const title = req.body.title;
+    const description=req.body.description;
+    const price = req.body.price;
+    const imageLink=req.body.imageLink;
+
+    const newCourse= await Course.create({
+        title,
+        description,
+        price,
+        imageLink
+    })
+
+    console.log(newCourse);
+
+    res.json({
+        message:"Course created successfully",
+        courseID:newCourse._id
+
+    })
+    
+
+});
+
+router.get('/courses', adminMiddleware, (req, res) => {
+    // Implement fetching all courses logic
+
+     Course.find({})
+        .then(function(response){
+            res.json({
+                courses : response 
+            })
+        })
+
+
+});
+
+
+
+module.exports = router;
---
+++ Backend/Routes/User.js
@@ -0,0 +1,80 @@
+const mongoose = require('mongoose');
+const { Router } = require("express");
+const router = Router();
+const userMiddleware = require("../middleware/user");
+const {User} = require("../db");
+const {Course} = require("../db");
+
+// User Routes
+router.post('/signup', async(req, res) => {
+    // Implement user signup logic
+    const username= req.body.username;
+    const password=req.body.password;
+
+    await User.create({
+        username:username,
+        password:password
+    })
+    res.json({
+            message : "User created successfully"
+    })
+    
+});
+
+router.get('/courses', (req, res) => {
+    // Implement listing all courses logic
+    Course.find({})
+    .then(function(response){
+        res.json({
+            courses : response 
+        })
+    })
+
+
+
+
+});
+
+router.post('/courses/:courseId', userMiddleware, (req, res) => {
+    // Implement course purchase logic
+
+    const courseID = req.params.courseId;
+    const username = req.headers.username;
+    User.updateOne({
+        username : username
+    },{
+        "$push":{
+            purchasedCourses:new mongoose.Types.ObjectId(courseID)
+        }
+    }).catch(function(e){
+        console.log(e);
+    });
+    res.json({
+
+        "message" : "Purchased successful"
+    })
+});
+
+router.get('/purchasedCourses', userMiddleware, async(req, res) => {
+    // Implement fetching purchased courses logic
+
+    const user = await User.findOne({
+        username: req.headers.username
+    });
+   
+    console.log(user.purchasedCourses);
+    const courses = await Course.find({
+        _id: {
+            "$in": user.purchasedCourses
+        }
+    });
+
+    res.json({
+        courses: courses
+    })
+
+
+
+});
+
+module.exports = router
